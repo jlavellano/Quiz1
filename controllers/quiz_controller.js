a81f744200b2edc8744dca60e51e78782dadff03
José Luis Avellano <jlavellano@gmail.com>
2015-07-26 00:08:07 +0200

    Añadido autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,17 @@
 var models = require('../models/models.js');
 
-// GET /quices/:id
-exports.show = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-      res.render('quizes/show', {quiz: quiz})
-  })
-};
-
-// GET /quizes/:id/answer
-exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		if (req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer', 
-				       {quiz: quiz, respuesta: 'Correcto'});
+// Autoload -factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId=' + quizId));
+			}
 		}
-		else {
-			res.render('quizes/answer', 
-				       {quiz: quiz, respuesta: 'Incorrecto'});
-		}
-	})
+	).catch(function(error) { next(error);});
 };
 
 // GET /quizes
@@ -26,4 +19,19 @@ exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes) {
 		res.render('quizes/index.ejs', { quizes: quizes});
 	})
+};
+
+// GET /quices/:id
+exports.show = function(req, res) {
+      res.render('quizes/show', { quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer', 
+       {quiz: req.quiz, respuesta: resultado});
 };
