62cb2fb6a701db05b297b68580cca2f696e8aed9
José Luis Avellano <jlavellano@gmail.com>
2015-08-02 02:07:27 +0200

    Añadida función crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,11 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load); // autoload
 
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)',   quizController.show);
+router.get('/quizes',                        quizController.index);
+router.get('/quizes/:quizId(\\d+)',          quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
+router.get('/quizes/new',                    quizController.new);
+router.post('/quizes/create',                quizController.create);
 
 
 router.get('/author', function(req, res) {
